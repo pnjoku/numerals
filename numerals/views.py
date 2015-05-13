b2806ff846af8bf61b1e4cf0583269563f85dfd2
Prince Njoku <princenjoku55@gmail.com>
2015-05-13 17:23:19 -0400

    update view

--- numerals/views.py
+++ numerals/views.py
@@ -1,14 +1,13 @@
-from forms import NumeralForm
-from django.shortcuts import render_to_response, redirect
-from django.template import RequestContext
+from django.shortcuts import render
+
+from .forms import NumeralForm
 
 
 def index(request):
+    form = NumeralForm(request.POST or None)
+    numerals = ''
     if request.method == 'POST':
-        form = NumeralForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('questions')
-    else:
-        form = NumeralForm()
-    return render_to_response('template/index.html', {'form': form}, context_instance=RequestContext(request))
+            numerals = form.to_roman()
+    return render(request, 'index.html', {'form': form,
+        'numerals': numerals})
