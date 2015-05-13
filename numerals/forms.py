fc44547df803b317041921198feb82e3614ccc7d
Prince Njoku <princenjoku55@gmail.com>
2015-05-13 15:31:09 -0400

    Add form class template and view files

---
+++ numerals/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.utils import OutOfRangeError, NotIntegerError
+
+#Define digit mapping
+romanNumeralMap = (('M',  1000),
+                   ('CM', 900),
+                   ('D',  500),
+                   ('CD', 400),
+                   ('C',  100),
+                   ('XC', 90),
+                   ('L',  50),
+                   ('XL', 40),
+                   ('X',  10),
+                   ('IX', 9),
+                   ('V',  5),
+                   ('IV', 4),
+                   ('I',  1))
+
+
+def toRoman(n):
+    """convert integer to Roman numeral"""
+    if not (0 < n < 4000):
+            raise OutOfRangeError("number out of range (must be 1..3999)")
+    if int(n) != n:
+            raise NotIntegerError("non-integers can not be converted")
+
+    result = ""
+    for numeral, integer in romanNumeralMap:
+        while n >= integer:
+            result += numeral
+            n -= integer
+    return result
