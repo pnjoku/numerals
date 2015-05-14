c827f151fe99d04bba20a6a87d2ef19793d6b99e
Prince Njoku <princenjoku55@gmail.com>
2015-05-14 10:56:21 -0400

    update implementation of roman numeral map

--- numerals/forms.py
+++ numerals/forms.py
@@ -1,24 +1,10 @@
 from django import forms
 
+from .constants import ROMAN_NUMERAL_MAP
+
 
 class NumeralForm(forms.Form):
     n = forms.IntegerField(min_value=1, max_value=3999)
-
-    ROMAN_NUMERAL_MAP = (
-        ('M',  1000),
-        ('CM', 900),
-        ('D',  500),
-        ('CD', 400),
-        ('C',  100),
-        ('XC', 90),
-        ('L',  50),
-        ('XL', 40),
-        ('X',  10),
-        ('IX', 9),
-        ('V',  5),
-        ('IV', 4),
-        ('I',  1)
-    )
 
     def to_roman(self):
         """convert integer to Roman numeral"""
@@ -26,7 +12,7 @@ class NumeralForm(forms.Form):
         n = self.cleaned_data.get('n')
 
         result = ""
-        for numeral, integer in self.ROMAN_NUMERAL_MAP:
+        for numeral, integer in ROMAN_NUMERAL_MAP:
             while n >= integer:
                 result += numeral
                 n -= integer
