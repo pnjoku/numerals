1ea6f338e3cfc0dc74222ff1b431da4fd207dc59
Prince Njoku <princenjoku55@gmail.com>
2015-05-14 10:58:00 -0400

    update test.py to assert that items from roman numeral map are being used properly

--- numerals/tests.py
+++ numerals/tests.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
-from .forms import to_roman
+
+from .constants import ROMAN_NUMERAL_MAP
+from .forms import NumeralForm
 
 
 class NumeralTests(TestCase):
-    def testToRoman(self):
-        """toRoman(n) for all n"""
-        for integer in range(1, 4000):
-            numeral = to_roman(integer)
-            self.assertEqual(numeral, numeral)
+
+    def test_to_roman(self):
+
+        for item in ROMAN_NUMERAL_MAP:
+            form = NumeralForm({'n': item[1]})
+            form.is_valid()
+            numeral = form.to_roman()
+            self.assertEqual(numeral, item[0],
+                "Integer was not converted correctly. "
+                "{} was expected to be {}. Instead "
+                "was {}".format(
+                    item[1], item[0], numeral))
